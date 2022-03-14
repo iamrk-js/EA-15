d16e13fb69babdd3a864880e4e73aa201d756646
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2022-03-14 11:44:50 -0700

    hof

--- Array/Array methods/assets/js/app.js
+++ Array/Array methods/assets/js/app.js
@@ -42,6 +42,87 @@ cl(revArr);
 // 5lmtH
 
 
+var arr = ["Html5", "CSS3", "javaScript", "es6", "Typescript", "Angular"];
+// pop >> to remove last(last index) element of array
+arr.pop();
+cl(arr);
+// push >> to add the element at the end(last Index) of array
+arr.push("Angular 13");
+cl(arr);
+
+// shift >>to remove 1st(0th index) element of the array
+
+arr.shift();
+
+cl(arr);
+
+// unshift >> to add the element at the start(0th index) of array
+
+arr.unshift('HTML5');
+
+cl(arr);
+
+// merging/concat
+
+var skill = ["Node.js", "express", 'mongoDB'];
+
+// let MEANstack = arr.concat(skill);
+let MEANstack = arr.concat(skill, 'github', 'jira');
+
+cl(MEANstack);
+
+
+var arr = ["Html5", "CSS3", "javaScript", "Photoshop", "es6", "Typescript", "Angular"];
+
+delete arr[3];
+
+cl(arr)
+
+var arr = ["Html5", "CSS3", "javaScript", "Photoshop", "es6", "Typescript", "Angular"];
+
+// splice(start:number, deleteCount?:number)
+
+// arr.splice(3,1)
+// arr.splice(3) >> If we don't pass secont parameter all elemets from
+// 1st parameter will be removed
+
+// arr.splice(3,1,"SASS","Bootstrap");
+
+arr.splice(3,0,"SASS","Bootstrap");
+
+cl(arr)
+//slice 
+
+var arr = ["Html5", "CSS3", "javaScript", "Photoshop", "es6", "Typescript", "Angular"];
+// slice >> It returns a new array
+// It accepts 2 arrguments and both are not mandetory
+let newSkill = arr.slice(4,6);
+
+cl(newSkill);
+newSkill = arr.slice(4);
+
+cl(newSkill);
+// by default value of 1st arrgument is 0
+newSkill = arr.slice();
+
+cl(newSkill);
+
+arr.pop();
+cl(arr);
+cl(newSkill);
+
+// var arr2 = arr;
+
+// cl(arr2)
+// arr.pop();
+
+// cl(arr)
+// cl(arr2)
+
+
+
+
+
 
 
 
